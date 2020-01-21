2d5e324d2eb5b0c255c8ebe058fb81de4b9acdd7
Michael Aylesbury <michael@michaels-mbp.lan>
2020-01-21 17:44:54 +0000

    Finished base game

--- main.py
+++ main.py
@@ -19,13 +19,63 @@ def draw(array):
 
 
 def valid(x, y, n, board):
-    if n not in range(1, 9) or n in board[:, x] or n in board[y, :] or y not in range(9) or x not in range(9):
-        print("input invalid")
+    if n not in range(1, 9):
+        print("Error: n not between 1 and 9")
         return False
+    elif n in board[:, x]:
+        print("Error: n already in column")
+        return False
+    elif n in board[y, :]:
+        print("Error: n already in row")
+        return False
+    elif y not in range(9):
+        print("Error: coordinates out of bounds")
+        return False
+    elif x not in range(9):
+        print("Error: coordinates out of bounds")
+        return False
+    elif 0 <= y <= 2:
+        if 0 <= x <= 2:
+            if n in board[0:3, 0:3]:
+                print("Error: n already in sub-grid")
+                return False
+        if 3 <= x <= 5:
+            if n in board[3:6, 0:3]:
+                print("Error: n already in sub-grid")
+                return False
+        if 6 <= x <= 9:
+            if n in board[6:9, 0:3]:
+                print("Error: n already in sub-grid")
+                return False
+    elif 3 <= y <= 5:
+        if 0 <= x <= 2:
+            if n in board[0:3, 3:6]:
+                print("Error: n already in sub-grid")
+                return False
+        if 3 <= x <= 5:
+            if n in board[3:6, 3:6]:
+                print("Error: n already in sub-grid")
+                return False
+        if 6 <= x <= 9:
+            if n in board[6:9, 3:6]:
+                print("Error: n already in sub-grid")
+                return False
+    elif 6 <= y <= 9:
+        if 0 <= x <= 2:
+            if n in board[0:3, 6:10]:
+                print("Error: n already in sub-grid")
+                return False
+        if 3 <= x <= 5:
+            if n in board[3:6, 6:10]:
+                print("Error: n already in sub-grid")
+                return False
+        if 6 <= x <= 9:
+            if n in board[6:9, 6:10]:
+                print("Error: n already in sub-grid")
+                return False
     else:
         return True
-# check the subgrid
-  
+
 
 def complete(board):
     if 0 not in board:
