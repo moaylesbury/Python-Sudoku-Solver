7d3d1f9ef86f9c874c3c65f3baf58c675952c0e7
Michael <moaylesbury@gmail.com>
2020-01-29 00:15:46 +0000

    Game finished, changes and refactoring

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import numpy as np
 import tkinter as tk
+from random import randint
 
 
-class Sudoku:
+class Board:
     def __init__(self):
         self.board = np.zeros((9, 9), dtype=int)
 
 
-def gridrange(x):
+def grid_range(x):
     return [0, 3] if 0 <= x <= 2 else [3, 6] if 3 <= x <= 5 else [6, 9] if 6 <= x <= 9 else [0, 0]
 
 
@@ -18,33 +19,9 @@ class Interface(tk.Frame, object):
         self.col, self.row = 0, 0
         self.master = master
         self.game = game
-        self.__display()
-        self.pack()
+        self.original_board, self.solved = np.zeros((9, 9), dtype=int), np.zeros((9, 9), dtype=int)
 
-    def __display(self):
         self.dimension = 512  # just called dimension as height = width in the square grid
-        self.master.title("Sudoku")
-        self.pack(fill="both", expand=1)
-
-        self.canvas = tk.Canvas(self, width=self.dimension, height=self.dimension)
-        self.canvas.pack(fill="both", side="top")
-
-        quit_button = tk.Button(self, text="Quit", fg="red", command=self.master.destroy)
-        quit_button.pack(fill="both", side="bottom")
-
-        clear_button = tk.Button(self, text="Clear Board", command=self.clear)
-        clear_button.pack(fill="both", side="bottom")
-
-        solve_button = tk.Button(self, text="Solve", command=self.solving_algorithm)
-        solve_button.pack(fill="both", side="bottom")
-
-        self.__create_grid()
-
-        self.canvas.bind("<Button-1>", self.__click)  # add a function for clicking
-        self.canvas.bind("<Key>", self.__key_press)  # add a function for key pressing
-
-    def __create_grid(self):
-        # Defining constants
         self.increment = 50
         self.side_padding = (self.dimension - 9 * 50) // 2
         self.top_padding = 35
@@ -52,6 +29,62 @@ class Interface(tk.Frame, object):
         self.right_edge = self.dimension - self.side_padding
         self.bottom_edge = 9 * 50 + self.top_padding
 
+        self.xs, self.ys = [], []
+        for i in range(9):
+            [self.xs.append(x) for x in [56 + self.increment * i for i in range(9)]]
+        vy = [60 + self.increment * j for j in range(9)]
+        for v in vy:
+            for i in range(9):
+                self.ys.append(v)
+
+        self.__display()
+        self.new_board()
+        self.pack()
+
+    def __display(self):
+
+        self.master.title("Sudoku")
+        self.pack(fill="both", expand=1)
+
+        self.canvas = tk.Canvas(self, width=self.dimension, height=self.dimension, highlightthickness=0)
+        self.canvas.pack(fill="both", side="top")
+
+        quit_button = tk.Button(self, text="Quit", fg="red", command=self.master.destroy)
+        quit_button.pack(fill="both", side="bottom")
+
+        reset_button = tk.Button(self, text="Reset", command=self.reset_board)
+        reset_button.pack(fill="both", side="bottom")
+
+        new_button = tk.Button(self, text="New Sudoku", command=self.new_board)
+        new_button.pack(fill="both", side="bottom")
+
+        solve_button = tk.Button(self, text="Solve", command=self.solve)
+        solve_button.pack(fill="both", side="bottom")
+
+        self.__create_grid()
+
+        self.canvas.bind("<Button-1>", self.__click)  # add a function for clicking
+        self.canvas.bind("<Key>", self.key_press)  # add a function for key pressing
+
+    def new_board(self):
+        self.game.board = np.zeros((9, 9), dtype=int)
+        self.solving_algorithm()
+        self.solved = np.copy(self.game.board)
+        remove = randint(36, 46)
+        while remove != 0:
+            x, y = randint(0, 8), randint(0, 8)
+            if self.game.board[x][y] != 0:
+                self.game.board[x][y] = 0
+                remove -= 1
+        self.original_board = np.copy(self.game.board)
+        self.draw()
+
+    def reset_board(self):
+        self.game.board = self.original_board
+        self.draw()
+
+    def __create_grid(self):
+        # Defining constants
         self.canvas.create_line(self.side_padding, self.top_padding, self.side_padding, self.bottom_edge, fill="blue")
         self.canvas.create_line(self.side_padding, self.top_padding, self.right_edge, self.top_padding, fill="blue")
         for i in range(1, 10):
@@ -63,29 +96,31 @@ class Interface(tk.Frame, object):
         self.draw()
 
     def draw(self):
-        xs = []
-        ys = []
-
         # Clearing text
         self.canvas.delete("numbers")
-
-        # Preparing coords
-        for i in range(9):
-            [xs.append(x) for x in [56 + self.increment * i for i in range(9)]]
-        vy = [60 + self.increment * j for j in range(9)]
-        for v in vy:
-            for i in range(9):
-                ys.append(v)
-
+        print(self.solved)
         # Drawing
         count = 0
-        for row in self.game.board:
-            for entry in row:
-                self.canvas.create_text(xs[count], ys[count], text=entry, tags="numbers")
+        for x in range(9):
+            for y in range(9):
+                if not self.finished():
+                    if self.original_board[x][y] == 0:
+                        self.canvas.create_text(self.xs[count], self.ys[count], text=self.game.board[x][y],
+                                                tags="numbers", fill="black")
+                    else:
+                        self.canvas.create_text(self.xs[count], self.ys[count], text=self.original_board[x][y],
+                                                tags="numbers", fill="blue")
+                else:
+                    self.canvas.create_text(self.xs[count], self.ys[count], text=self.game.board[x][y],
+                                            tags="numbers", fill="green")
                 count += 1
 
     def __click(self, event):
-        # TODO: Check if game is over
+
+        if self.finished():
+            self.draw()
+            return
+
         self.canvas.delete("selected")
         yaxis = [self.top_padding + 50 * i for i in range(10)]
         xaxis = [self.side_padding + 50 * i for i in range(10)]
@@ -103,22 +138,23 @@ class Interface(tk.Frame, object):
                 y1 = yaxis[i + 1]
         self.canvas.create_rectangle(x0, y0, x1, y1, outline="red", tags="selected")
 
-    def __key_press(self, event):
-        # TODO: Check if game is over
-        if self.valid(int(event.char), self.row, self.col):
+    def key_press(self, event):
+
+        if self.finished():
+            self.draw()
+            return
+
+        if self.valid(self.row, self.col, int(event.char)):
             self.game.board[self.col][self.row] = event.char
             self.draw()
 
-    def clear(self):
-        self.game.board = np.zeros((9, 9), dtype=int)
-        self.draw()
-
-    def valid(self, y, x, n):
+    def valid(self, x, y, n):
         if n not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or n in [r for r in self.game.board[:, x]] \
                 or n in [c for c in self.game.board[y, :]]:
             return False
 
-        rr, rc = gridrange(x), gridrange(y)
+        rr, rc = grid_range(x), grid_range(y)
+
         if n in [i for i in np.reshape(self.game.board[rc[0]:rc[1], rr[0]:rr[1]], 9)]:
             return False
 
@@ -133,32 +169,34 @@ class Interface(tk.Frame, object):
                 if self.game.board[x][y] == 0:
                     return x, y
 
+    def solve(self):
+        self.game.board = self.solved
+        self.draw()
+
     def solving_algorithm(self):
         # Returns true if the algorithm is successful, false otherwise
-
         if self.finished():
             return True
 
         x, y = self.empty()
 
         for n in range(1, 10):
-            if self.valid(x, y, n):
+
+            if self.valid(y, x, n):
                 self.game.board[x][y] = n
-                self.draw()
 
                 if self.solving_algorithm():
                     return True
 
                 self.game.board[x][y] = 0
-                self.draw()
 
         return False
 
 
 if __name__ == '__main__':
-    game = Sudoku()
-    game.__init__()
+    Game = Board()
+    Game.__init__()
 
     root = tk.Tk()
-    Interface = Interface(game, root)
+    Interface = Interface(Game, root)
     root.mainloop()
